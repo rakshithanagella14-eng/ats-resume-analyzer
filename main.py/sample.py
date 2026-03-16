f3cf588f779e03109a85c53380b62cbbc1d25f66
RakshiEce <rakshithanagella14@mail.com>
2026-03-16 14:07:16 +0530

    Added skill detection from resume text

--- main.py/sample.py
+++ main.py/sample.py
@@ -1,4 +1,27 @@
 import PyPDF2
+skills_db = [
+    "python",
+    "java",
+    "c++",
+    "machine learning",
+    "data analysis",
+    "sql",
+    "git",
+    "flask",
+    "django",
+    "matlab",
+    "iot",
+    "embedded systems"
+]
+def detect_skills(resume_text):
+    resume_text = resume_text.lower()
+    detected = []
+
+    for skill in skills_db:
+        if skill in resume_text:
+            detected.append(skill)
+
+    return detected
 
 def extract_resume_text(file_path):
     with open(file_path, "rb") as file:
@@ -15,3 +38,8 @@ resume_text = extract_resume_text("text_resume.pdf")
 
 print("Resume Content:\n")
 print(resume_text)
+skills_found = detect_skills(resume_text)
+
+print("\nDetected Skills:")
+for skill in skills_found:
+    print(skill)
